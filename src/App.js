2d64016b807087d0b3fc31127df3c2f129fbfbc1
tavsheng <tavsheng@yandex.ru>
2021-11-13 18:57:23 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [buttonStatus, setButtonStatus] = useState(false);
+  const [buttonNumber, setButtonNumber] = useState(1);
+
+  const button2Click = () => {setButtonNumber(2)}
+  const button3Click = () => {setButtonNumber(3)}
+  const buttonF1Click = () => {setButtonStatus(value => !value)}
+  const buttonF2Click = () => {setButtonNumber(1)}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=>{ buttonF1Click(); buttonF2Click() }} >{buttonNumber}</button>
+     {buttonStatus
+     ? <div><button onClick = {buttonF2Click}>1</button>
+     <button onClick = {button2Click}>2</button>
+     <button onClick = {button3Click}>3</button>
+     </div>
+      :null
+      
+  }
     </div>
   );
 }
